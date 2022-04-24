341d575a284cc6030de1f09a3fc44cfc0ec79bdd
hajratariqi <103792958+hajratariqi@users.noreply.github.com>
2022-04-25 04:17:28 +0500

    Add files via upload

---
+++ on-off-bulb/app.js
@@ -0,0 +1,8 @@
+function onBuilb(){
+    var builb = document.getElementById("builb");
+    builb.src = "on.jpg"
+}
+function offBuilb(){
+    var builb = document.getElementById("builb");
+    builb.src = "off.png"
+}
